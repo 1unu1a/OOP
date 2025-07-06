6e4c4a5c661dbcb43fee7f28cf8aa9fe54f79b6e
1unu1a <127436545+1unu1a@users.noreply.github.com>
2025-07-06 21:19:16 +0300

    Initial commit

---
+++ Player.cs
@@ -0,0 +1,23 @@
+namespace My.Home.Work.Oop;
+
+public class Player //Работа с классами
+{
+    private string name;
+    private int ievel;
+    private int health;
+
+    public Player(string name, int ievel, int health)
+    {
+        this.name = name;
+        this.ievel = ievel;
+        this.health = health;
+    }
+
+    public void PrintInfo()
+    {
+        Console.WriteLine("Информация об игроке:");
+        Console.WriteLine("Имя: {0}", this.name);
+        Console.WriteLine("Уровень: {0}", this.ievel);
+        Console.WriteLine("Здоровье: {0}", this.health);
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace My.Home.Work.Oop
+{
+    internal class Program //Блок ООП
+    {
+        static void Main(string[] args)
+        {
+            Player player = new Player("Коля", 17, 99);
+            player.PrintInfo();
+        }
+    }
+}
